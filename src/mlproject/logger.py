ec72895817e27acc8fa7774f687732baa0679432
Mohsin678 <mohsin79.h@gmail.com>
2025-04-21 17:58:33 +0530

    added logging exception file

---
+++ src/mlproject/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+Log_file = f"{datetime.now().strftime("")}.log"
+Log_path = os.path.join(os.getcwd(),"logs",Log_file)
+os.makedirs(Log_path,exist_ok=True)
+
+Log_file_path = os.path.join(Log_path,Log_file)
+logging.basicConfig(
+    filename=Log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+if __name__ =="__main__":
+    logging.info("logging has started")
